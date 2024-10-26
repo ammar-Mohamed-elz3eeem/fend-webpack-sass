f214ec41e4ce5588b961ba46a75f35b9777d7449
ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2024-10-26 16:21:48 +0300

    Add optimization plugins to production webpack configurations

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,20 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
 	entry: "./src/client/index.js",
 	mode: "production",
+	output: {
+		libraryTarget: "var",
+		library: "Client",
+	},
+	optimization: {
+		minimizer: [new TerserWebpackPlugin(), new OptimizeCssAssetsWebpackPlugin()]
+	},
 	module: {
 		rules: [
 			{
@@ -13,8 +23,8 @@ module.exports = {
 				loader: "babel-loader",
 			},
 			{
-				test: /\.scss$/,
-				use: ["style-loader","css-loader","sass-loader"],
+				test: /\.(scss|sass)$/,
+				use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
 			},
 		],
 	},
@@ -22,6 +32,9 @@ module.exports = {
 		new HtmlWebPackPlugin({
 			template: "./src/client/views/index.html",
 			filename: "./index.html",
+		}),
+		new MiniCssExtractPlugin({
+			filename: "[name].css"
 		}),
 	],
 };
