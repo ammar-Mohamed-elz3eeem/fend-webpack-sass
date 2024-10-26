5847e2ba999aa7466acc42e4103655307789c09f
ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2024-10-26 15:44:26 +0300

    Fix conflicting issues with js to make library for client side code

--- src/client/index.js
+++ src/client/index.js
@@ -6,7 +6,10 @@ import './styles/header.scss';
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
-console.log(checkForName);
-
-alert("I EXIST")
+// alert("I EXIST")
 console.log("CHANGE!!");
+
+export {
+  checkForName,
+  handleSubmit
+}
